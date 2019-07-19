6643d4aaa8d0af584bca302456d6a22da4c45c7a
Luo Huan <1026770043@qq.com>
2019-07-19 20:23:19 +0800

    first

---
+++ code/ddmail_parent/ddmail_shop_web/src/test/java/test01.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Elves
+ * @Description:
+ * @Date: Created in 20:06 2019/7/19
+ * @Modified By:
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println(0);
+    }
+}
